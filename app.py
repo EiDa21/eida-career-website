0de1e28d39032ee79e6af190770c3a48d6c672e2
EiDa21 <101800570+EiDa21@users.noreply.github.com>
2023-08-15 17:45:12 +0000

    Added Jobitem.html

--- app.py
+++ app.py
@@ -3,10 +3,31 @@ from flask import render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Abuja',
+  'Salary': '$15,000',
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Lagos',
+  'Salary': '$17,000',
+}, {
+  'id': 3,
+  'title': 'Software Engineer',
+  'location': 'Abuja',
+  'Salary': '$35,000',
+}, {
+  'id': 4,
+  'title': 'Front-end Engineer',
+  'location': 'Remote',
+}]
+
 
 @app.route("/")
 def helloWorld():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Jovia')
 
 
 if __name__ == '__main__':
